562a9d469bcf44da481107f5b66cba348791750b
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-02-25 22:05:11 +0300

    Scroll

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -15,7 +15,14 @@ class App extends Component{
     }
 
     componentDidMount(){
-        this.setState({robots:robots})
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>{
+            return response.json();
+        })
+        .then(users=>{
+            this.setState({robots:users})
+        })
+        
     }
 
     onSearchChange= (event) => {
@@ -26,14 +33,22 @@ class App extends Component{
         const filteredRobots=this.state.robots.filter(robot=>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-            
-        )
+        if(this.state.robots.length===0){
+            return <h1>Loading</h1>
+        }
+        else{
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                       <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+                
+            )
+        }
+       
     }
   
 }
